a4e20604abb67717584ad6e03940371159477bbf
Povor_ID <neotropil@gmail.com>
2024-07-30 06:14:18 +0300

    Delete kotlin-m5-03 directory

--- kotlin-m5-03/Season.kt
+++
@@ -1,9 +0,0 @@
-
-class Season{
-    fun getSeason(month: Int): String {
-        if (month in 3..5) return " - весна."
-        else if (month in 6..8) return " - лето."
-        else if (month in 9..11) return " - осень."
-        else return " - зима."
-    }
-}

3340fadcba347a809a19a71f08cfd9858a50cc5e
Povor_ID <neotropil@gmail.com>
2024-07-30 06:14:03 +0300

    Delete kotlin-m5-02 directory

--- kotlin-m5-02/Ticket.kt
+++
@@ -1,8 +0,0 @@
-import java.sql.Time
-
-data class Ticket(val num: Int, val name: String, val time: String, val row: Int, val seat: Int) {
-
-    override fun toString(): String {
-        return "Билет №$num: '$name' $time ряд:$row место:$seat"
-    }
-}

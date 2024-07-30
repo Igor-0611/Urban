24559a30207f6f925c9b89750a75d08e9244b0bc
Povor_ID <neotropil@gmail.com>
2024-07-30 06:03:47 +0300

    Delete kotlin-m3-01.kt

--- kotlin-m3-01.kt
+++
@@ -1,9 +0,0 @@
-
-fun main() {
-    val age: Int = 25
-    val name: String = "Igor"
-    val isStudent: Boolean = true
-    val salary: Double = 50000.0
-
-    println("Age: " + age + " | Name: " + name + " | Is Student: " + isStudent + " | Salary: " + salary)
-}

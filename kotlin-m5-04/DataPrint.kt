2ce523dbfd395a5007785259783e3de88c41cdce
Povor_ID <neotropil@gmail.com>
2024-07-30 06:14:31 +0300

    Delete kotlin-m5-04 directory

--- kotlin-m5-04/DataPrint.kt
+++
@@ -1,3 +0,0 @@
-
-class DataPrint(dev: String, prt: String, col: Boolean, name: String) : Printer(dev, prt, col, name) {
-}
--- kotlin-m5-04/Printer.kt
+++
@@ -1,9 +0,0 @@
-
-abstract class Printer(val dev: String, val prt: String, val col: Boolean, val name: String) {
-
-    open fun infoPrinter() {
-        var color =  "цветной"
-        if (!col) color = "ч/белый"
-        println("$dev $prt $color <$name>")
-    }
-}

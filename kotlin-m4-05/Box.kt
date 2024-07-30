8edd1fc6fbd2e4d213daae78cf3ff401decbc5fb
Povor_ID <neotropil@gmail.com>
2024-07-30 06:09:58 +0300

    Delete kotlin-m4-05 directory

--- kotlin-m4-05/Box.kt
+++
@@ -1,7 +0,0 @@
-class Box(val width:Double, val height:Double, val depth:Double){
-
-    fun vOfBox():String {
-        var V = width * height * depth
-        return "Объём коробки V = $V куб.м."
-    }
-}

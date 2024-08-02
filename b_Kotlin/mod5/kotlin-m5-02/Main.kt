5cf4fada70b11f72f5b78f2356fad1e5be1b00bc
Povor_ID <neotropil@gmail.com>
2024-08-02 18:31:12 +0300

    Add files via upload

--- b_Kotlin/mod5/kotlin-m5-02/Main.kt
+++ b_Kotlin/mod5/kotlin-m5-02/Main.kt
@@ -1,11 +1,12 @@
 
 fun main() {
-    println()
+
     var num = 0
     val name = "Адреналин"
     val time = "18:00"
-    var row = 0
-    var seat = 0
+    var row = -1
+    var seat = -1
+    var tmp = ""
     val ticket = Ticket(num, name, time, row, seat)
     val hall: Array<Array<Ticket>>  = Array(3, { Array(5, {ticket}) })
 
@@ -18,8 +19,10 @@ fun main() {
     }
 
     var sale = true
+
     do {
-        println("Выбирете свободное место:")
+        println("\n $name $time")
+        println("выбирете свободное место:")
         for (i in 0 until hall.size) {
             print("ряд ${i + 1}: ")
             for (j in 0 until hall[i].size) {
@@ -30,14 +33,34 @@ fun main() {
             }
             println()
         }
-        print("укажите ряд - ")
-        row = readln().toInt()
-        print("укажите место - ")
-        seat = readln().toInt()
-        hall[row-1][seat-1] = Ticket(++num, name, time, row, seat)
+
+        do {
+            print("укажите ряд - ")
+            tmp = readln()
+            row = check(tmp, hall.size)
+        } while (row < 0)
+
+        do {
+            print("укажите место - ")
+            tmp = readln()
+            seat = check(tmp, hall[0].size)
+
+            if (seat >= 0 && hall[row][seat].num != 0) {
+                print("место уже занято / ")
+                seat = -1
+            }
+        } while (seat < 0)
+
+        hall[row][seat] = Ticket(++num, name, time, row, seat)
+        println()
+        println(hall[row][seat].toString())
+        row = 0
+        seat = 0
+
+        println()
         print("Продолжить продажи нет.0/да.1 - ")
-        if (readln().toInt() == 0) sale = false
-    }while(sale)
+        if (readln() == "0") sale = false
+    } while(sale)
 
     println()
     println("Список проданных билетов")
@@ -46,13 +69,19 @@ fun main() {
             if (hall[i][j].num > 0) println(hall[i][j].toString())
         }
     }
+}
 
-    println()
-    print("Распечатать билет № ")
-    var pnt = readln().toInt()
-    for (i in 0 until hall.size) {
-        for (j in 0 until hall[i].size) {
-            if (hall[i][j].num == pnt) println(hall[i][j].toString())
+fun check (opt: String, lmt: Int): Int {
+    if (opt.length > 1 || !opt[0].isDigit()) {
+        println("плохой выбор / ")
+        return -1
+    }
+    else {
+        val chc = opt.toInt()
+        if (chc < 1 || chc > lmt) {
+            print("плохой выбор / ")
+            return -1
         }
+        else return (chc - 1)
     }
 }
--- b_Kotlin/mod5/kotlin-m5-02/Ticket.kt
+++ b_Kotlin/mod5/kotlin-m5-02/Ticket.kt
@@ -3,6 +3,6 @@ import java.sql.Time
 data class Ticket(val num: Int, val name: String, val time: String, val row: Int, val seat: Int) {
 
     override fun toString(): String {
-        return "Билет №$num: '$name' $time ряд:$row место:$seat"
+        return "Билет №$num: '$name' $time ряд: ${row+1} место: ${seat+1}"
     }
 }
